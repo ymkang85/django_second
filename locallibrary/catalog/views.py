66abcba42f9c64d475d445c0904289d381cb835d
ymkang85 <kangym855@gmail.com>
2024-04-29 11:23:53 +0900

    first_commit

---
+++ locallibrary/catalog/views.py
@@ -0,0 +1,33 @@
+from catalog.models import Book, Author, BookInstance
+from django.shortcuts import render
+from django.views import generic
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact = "a").count()
+
+    num_authors = Author.objects.count()
+
+    context = {
+        "num_books" : num_books,
+        "num_instances" : num_instances,
+        "num_instances_available" : num_instances_available,
+        "num_authors" : num_authors,
+    }
+
+    return render(request, "index.html", context = context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
